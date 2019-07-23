4e0d83a97616c0ffd69f02eed2db55142b4f4ffa
nerminKrdzic <nkrdzic1@etf.unsa.ba>
2019-07-23 23:43:39 +0200

    Gulp serve

--- frontend/gulp/build.js
+++ frontend/gulp/build.js
@@ -20,7 +20,7 @@ gulp.task('partials', function () {
       collapseWhitespace: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'vjezba',
+      module: 'frontend',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
